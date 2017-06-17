6f2650075d6bc9bdb4a9e43952a82c265c03c3ff
SAVR <https://github.com/yusufsafrudin/Lari.git>
2017-06-17 14:11:01 +0700

    Lari

--- app/src/main/java/com/example/savr/lari/Login.java
+++ app/src/main/java/com/example/savr/lari/Login.java
@@ -1,24 +1,46 @@
 package com.example.savr.lari;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.savr.lari.fragment.HomeFragment;
 
-public class Login extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextUsername,editTextEmail,editTextPassword;
+    private Button buttonRegister;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        final EditText etData1 = (EditText) findViewById(R.id.username);
+    /*  final EditText etData1 = (EditText) findViewById(R.id.username);
         final EditText etData2 = (EditText) findViewById(R.id.email);
         final EditText etData3 = (EditText) findViewById(R.id.password);
-        Button signup = (Button) findViewById(R.id.signup);
+        Button signup = (Button) findViewById(R.id.btnRegister);
+
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +52,64 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    */
+        editTextUsername = (EditText) findViewById(R.id.username);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+        buttonRegister = (Button) findViewById(R.id.btnRegister);
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonRegister.setOnClickListener(this);
+    }
+
+    private void registerUSer(){
+        final String email = editTextEmail.getText().toString().trim();
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        progressDialog.setMessage("Register user...");
+        progressDialog.show();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                Server.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            Toast.makeText(getApplicationContext(),jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.hide();
+                        Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("username",username);
+                params.put("email",email);
+                params.put("password",password);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonRegister)
+        registerUSer();
     }
 }
---
+++ app/src/main/java/com/example/savr/lari/Server.java
@@ -0,0 +1,12 @@
+package com.example.savr.lari;
+
+/**
+ * Created by SAVR on 17/06/2017.
+ */
+
+public class Server {
+
+    private static final String ROOT_URL = "http://192.168.43.222/android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+}
