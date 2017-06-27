a198bb7618c7cce29837119080c80edc7845e6d1
SAVR <https://github.com/yusufsafrudin/Lari.git>
2017-06-27 13:02:40 +0700

    Lari

--- app/src/main/java/com/example/savr/lari/Register.java
+++ app/src/main/java/com/example/savr/lari/Register.java
@@ -1,24 +1,18 @@
 package com.example.savr.lari;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
-import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.savr.lari.fragment.HomeFragment;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,7 +20,7 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Login extends AppCompatActivity implements View.OnClickListener {
+public class Register extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editTextUsername,editTextEmail,editTextPassword;
     private Button buttonRegister;
@@ -35,7 +29,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     /*  final EditText etData1 = (EditText) findViewById(R.id.username);
         final EditText etData2 = (EditText) findViewById(R.id.email);
         final EditText etData3 = (EditText) findViewById(R.id.password);
@@ -45,7 +39,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this,MainActivity.class);
+                Intent intent = new Intent(Register.this,MainActivity.class);
                 intent.putExtra("user",etData1.getText().toString());
                 intent.putExtra("email",etData2.getText().toString());
                 intent.putExtra("password",etData3.getText().toString());
@@ -103,8 +97,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
             }
         };
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
     }
 
     @Override
